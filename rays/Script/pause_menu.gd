501987d66da183a0ae05e6fb51599ed5639d1766
jahaa032 <jfhannestad@gmail.com>
2026-01-13 12:32:23 +0100

    Last push on the repo

--- rays/Script/pause_menu.gd
+++ rays/Script/pause_menu.gd
@@ -5,7 +5,7 @@ func _ready() -> void:
 	get_tree().paused = false
 
 func _input(event: InputEvent) -> void:
-	if Input.is_action_just_pressed("ui_cancel"):
+	if Input.is_action_just_pressed("Escape"):
 		if get_tree().paused:
 			visible = false
 			get_tree().paused = false
