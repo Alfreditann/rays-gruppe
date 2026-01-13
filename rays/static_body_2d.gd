2f951bd96e515138c4fae518b59687e72d65a1cb
jahaa032 <jfhannestad@gmail.com>
2026-01-13 10:10:59 +0100

    Animation added

--- rays/static_body_2d.gd
+++ rays/static_body_2d.gd
@@ -2,6 +2,12 @@ extends StaticBody2D
 
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
+	#1 Remove the monster when light hits it
 	if area.name == "Area2D":
-		queue_free()
-		
+		queue_free()	
+		#2 Change scene to win screen
+		get_tree().change_scene_to_file("res://tscn/win_screen.tscn")
+
+
+func _on_monster_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
